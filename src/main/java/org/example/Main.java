704633b279acb6389663d5cc4a722e5053fb95a9
desarrollo <desarrollo@academicos.cesde.edu.co>
2025-07-01 06:56:07 -0500

    feat: agregar variables

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,27 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner keyEntry=new Scanner(System.in);
+        System.out.println("***** RESIDENT EVIL CODE BY NO ONES *****");
+
+        //Personaje Leon S Kennedy
+        //nombre
+        //edad
+        //cantidadVida
+
+        //Personaje MR X (TITAN)
+        //nombre
+        //virus que lo infecto
+        //cantidadVIda
+        
+        byte edad;
+        String personaje1, personaje2, nombre, virus;
+        short cantidadVida;
 
 
     }
